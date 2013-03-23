4e222e58bd7a3256bd15e223ee9d71d8355211aa
Clerton <clertonleal@gmail.com>
2013-03-23 14:34:31 -0300

    Removed wrong copyrights

--- src/com/codeminer42/tracker/module/ManagerModuleConfiguration.java
+++ src/com/codeminer42/tracker/module/ManagerModuleConfiguration.java
@@ -1,11 +1,3 @@
-/*
- * Copyright 2012 Sagarana Tech.  All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * Sagarana Tech ("Confidential Information"). You shall not disclose such
- * Confidential Information and shall use it only in accordance with the
- * terms of the license agreement you entered into with Sagarana Tech.
- */
 package com.codeminer42.tracker.module;
 
 import com.google.inject.AbstractModule;
