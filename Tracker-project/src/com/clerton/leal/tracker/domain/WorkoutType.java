b5e5ae18bf17642996bc95370a4ec3158980f95a
clertonleal <clertonleal@sagaranatech.com>
2013-09-26 16:32:03 -0300

    commit 2

--- Tracker-project/src/com/clerton/leal/tracker/domain/WorkoutType.java
+++ Tracker-project/src/com/clerton/leal/tracker/domain/WorkoutType.java
@@ -3,12 +3,12 @@ package com.clerton.leal.tracker.domain;
 import android.R;
 
 
-
+asdfasdf
 /**
  * @author clertonleal@gmail.com
  * @since 23/03/2013
  * 
- * The constructor of this Enum receives the key of i18n value of WorkoutType.
+ * The constructor of this Enum receives the key of i18n value asdfasdfof WorkoutType.
  */
 public enum WorkoutType {
 
@@ -16,16 +16,16 @@ public enum WorkoutType {
 	SWIMMING(R.string.swimming),
 	BIKE(R.string.bike)
 	;
-	
+	asdfasdf
 	
 	private int resourceKey;
 	
 	private WorkoutType(final int resourceKey){
-		this.resourceKey = resourceKey;
+		this.resourceKey = resourceKey;asdfasdf
 	}
 	
 	public int getResourceKey(){
 		return resourceKey;
-	}
+	}asdfadf
 	
 }
