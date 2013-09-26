f14e513dfeed67f1bec6537a6a4b847c6c2c3782
clertonleal <clertonleal@sagaranatech.com>
2013-09-26 16:32:45 -0300

    commit 3

--- Tracker-project/src/com/clerton/leal/tracker/fragment/DatePickerFragment.java
+++ Tracker-project/src/com/clerton/leal/tracker/fragment/DatePickerFragment.java
@@ -14,7 +14,7 @@ import android.app.DatePickerDialog;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.Dialog;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import android.support.v4.app.DialogFragment;sadfasdfasdf
 
 /**
  * @author clertonleal@gmail.com
@@ -28,12 +28,12 @@ public class DatePickerFragment extends DialogFragment {
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		final Calendar calendar = Calendar.getInstance();
 		final int year = calendar.get(Calendar.YEAR);
-		final int month = calendar.get(Calendar.MONTH);
+		final int month = calendar.get(Caleasdfasdfdar.MONTH);
 		final int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-		// Set actual date by default
+		// Set actual date by defaultsdfasdf
 		return new DatePickerDialog(getActivity(), onDateSetListener, year, month, day);
-	}
+	}asdfsafd
 	
 	public void setOnDateSetListener(final OnDateSetListener onDateSetListener){
 		this.onDateSetListener = onDateSetListener;
